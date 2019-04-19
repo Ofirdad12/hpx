9f3ced178eff4b86961d7ee6d5a219e54977f5df
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2019-04-19 16:38:12 -0500

    Make the deprecation warnings issued by the old pp headers optional

--- libs/pp/include/hpx/util/detail/pp/cat.hpp
+++ libs/pp/include/hpx/util/detail/pp/cat.hpp
@@ -3,8 +3,11 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <hpx/pp/config/defines.hpp>
 #include <hpx/pp/cat.hpp>
 
+#if !defined(HPX_PP_HAVE_COMPATIBILITY_HEADERS)
 #pragma message(                                                               \
     "The header hpx/util/detail/cat.hpp is deprecated,                         \
     please include hpx/pp/cat.hpp instead")
+#endif
